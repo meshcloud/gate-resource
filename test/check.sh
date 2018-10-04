77c8740b7e855206f0f7937446151daa6f22ee31
Johannes Rudolph <jrudolph@meshcloud.io>
2018-10-04 16:22:52 +0200

    test(check): test check emits latest version only

--- test/check.sh
+++ test/check.sh
@@ -32,9 +32,19 @@ it_can_check_empty_gate() {
   '
 }
 
-# to do: test empty commit
-# todo: test consecutive commits emmit latest only
- 
+it_emits_latest_version_only() {
+  local repo=$(init_repo)
+  local ref=$(make_commit $repo)
+
+  local gate_ref1=$(make_commit_to_file $repo "my-gate/1234")
+  local gate_ref2=$(make_commit_to_file $repo "my-gate/abcd")
+
+  check_gate $repo "my-gate" | jq -e '
+    . == [{"gate": "my-gate", "passed": "abcd"}]
+  '
+}
+
 run it_can_check_from_head
 run it_can_check_empty_repo
 run it_can_check_empty_gate
+run it_emits_latest_version_only
